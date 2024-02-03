044d081506d00077aa3d29ff08cdd4b604933311
Jebear76 <jerepondumie@gmail.com>
2024-02-03 12:20:04 +0200

    ec2 lambda project diagram source

---
+++ EC2-Lambda-Architecture.py
@@ -0,0 +1,38 @@
+from diagrams import Diagram, Edge, Cluster
+from diagrams.aws.compute import EC2Ami, Lambda
+from diagrams.aws.network import VPC, PrivateSubnet, Endpoint
+from diagrams.aws.management import Cloudwatch
+from diagrams.aws.storage import S3
+
+
+with Diagram("EC2-Lambda", show=False):
+    cloudWatch = Cloudwatch("CloudWatch")
+    with Cluster("Amazon S3"):
+        codeBucket = S3("Code bucket")
+        inputBucket = S3("Input bucket")
+        outputBucket = S3("Output bucket")
+        buckets = [codeBucket, inputBucket, outputBucket]
+
+    with Cluster("AWS Lambda",direction="TB"):
+        triggerLambda = Lambda("Trigger Function")
+        shutdownLambda = Lambda("Cleanup Function")
+        lambdas = [triggerLambda, shutdownLambda]
+
+    with Cluster("ec2-lambda-vpc"):
+        with Cluster("private-subnet"):
+            s3Endpoint = Endpoint("S3 gateway endpoint")
+            lambdaEndpoint = Endpoint("Lambda service endpoint")
+            ec2Endpoint = Endpoint("EC2 service endpoint")
+
+            ami = EC2Ami("Preloaded AMI")
+            
+            buckets - s3Endpoint - Edge(label="Refresh code") - ami
+            s3Endpoint >> Edge(label="Get and Process files") << ami
+            s3Endpoint << Edge(label="Save Output") << ami            
+            ami << ec2Endpoint<< triggerLambda
+            ami >> lambdaEndpoint >> shutdownLambda
+
+            lambdas >> cloudWatch
+            ami >> cloudWatch
+
+    inputBucket >> Edge(label="Trigger") >> triggerLambda  
